bcda1f90c9a7f12f7b6b67f48c59590fb95814b4
Pre22 <pre.chika.22@gmail.com>
2020-09-07 18:26:26 +0100

    chore: added button widget

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -41,6 +41,7 @@ class Home extends StatelessWidget {
             )
           ]),
           PizzaImageWidget(),
+          OrderButton(),
         ]));
   }
 }
@@ -55,5 +56,31 @@ class PizzaImageWidget extends StatelessWidget {
       height: 400.0,
     );
     return Container(child: image);
+  }
+}
+
+class OrderButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var button = Container(
+      margin: EdgeInsets.only(top: 50.0),
+      child: RaisedButton(
+        child: Text("Order your Pizza!"),
+        color: Colors.lightGreen,
+        elevation: 5.0,
+        onPressed: () {
+          order(context);
+        },
+      ),
+    );
+    return button;
+  }
+
+  void order(BuildContext context) {
+    var alert = AlertDialog(
+      title: Text("Order Completed"),
+      content: Text("Thanks for your order"),
+    );
+    showDialog(context: context, builder: (BuildContext context) => alert);
   }
 }
